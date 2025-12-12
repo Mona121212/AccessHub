35820eb7f99fe0621b27a98e59b8e65809bd45ad
Mona <xiaomei.mona.he@gmail.com>
2025-12-11 17:15:46 -0700

    feat: change app.module

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersModule } from './modules/users/users.module';
+import { OrganizationsModule } from './modules/organizations/organizations.module';
+import { AuthModule } from './modules/auth/auth.module';
+
+// These User and Organization entities will be created later
+import { User } from './modules/users/user.entity';
+import { Organization } from './modules/organizations/organization.entity';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Global TypeORM configuration (ToolJet does this in server/ormconfig.ts + AppModule.register)
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432, // local Postgres setup
+      username: 'postgres',
+      password: 'MonaDatabase3355',
+      database: 'access_hub',
+      entities: [User, Organization],
+      synchronize: true, //Safe to use true in development, must disable in production
+    }),
+
+    // Business modules
+    UsersModule,
+    OrganizationsModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
